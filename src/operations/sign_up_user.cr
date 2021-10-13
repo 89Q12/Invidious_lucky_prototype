d84188a99f14fc67d1b2a1c86116e5085e65f1fc
11tuvork28 <zofi@xamh.de>
2021-10-13 22:54:54 +0200

    Removed sends email and renamed email to userid

--- src/operations/sign_up_user.cr
+++ src/operations/sign_up_user.cr
@@ -3,23 +3,17 @@ class SignUpUser < User::SaveOperation
   # Change password validations in src/operations/mixins/password_validations.cr
   include PasswordValidations
 
-  permit_columns email
+  permit_columns userid
   attribute password : String
   attribute password_confirmation : String
 
   before_save do
-    validate_uniqueness_of email
+    validate_uniqueness_of userid
     assign_confirmation_token
     Authentic.copy_and_encrypt password, to: encrypted_password
   end
 
-  after_commit send_confirmation_email
-
   private def assign_confirmation_token
     confirmation_token.value = Random::Secure.urlsafe_base64(32)
-  end
-
-  private def send_confirmation_email(user)
-    SignUpConfirmationEmail.new(user).deliver_later
   end
 end
