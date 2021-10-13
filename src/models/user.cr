b25d286c6734bf88c7dde6d050120cbb70aab53f
11tuvork28 <zofi@xamh.de>
2021-10-13 22:15:31 +0200

    user model now matches the invidious user model

--- src/models/user.cr
+++ src/models/user.cr
@@ -1,34 +1,14 @@
 class User < BaseModel
-  include Carbon::Emailable
   include Authentic::PasswordAuthenticatable
 
-  include Users::EmailConfirmable
-
   table do
-    column email : String
-    column first_name : String?
-    column last_name : String?
+    column userid : String
     column encrypted_password : String
-  end
-
-  def full_name : String?
-    return nil unless first_name || last_name
-
-    [first_name, last_name].compact.join(" ")
-  end
-
-  # Show Gravatar images by default for users.
-  #
-  # If there is another potential source, you can select among them in this method.
-  def image(size : Number = 100) : String
-    gravatar_base_url = "https://gravatar.com/avatar/"
-    hash = Digest::MD5.hexdigest(email.downcase)
-    params = "?s=#{size}&d=identicon"
-
-    gravatar_base_url + hash + params
-  end
-
-  def emailable : Carbon::Address
-    Carbon::Address.new(email)
+    notifications : String[]
+    subscriptions : String[]
+    preferences : String
+    token : String
+    watched : String[]
+    feed_needs_update : Boolean
   end
 end
