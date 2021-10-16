57fa7f821b5f1b71b9d1b2a6ca4fae5973f6e455
11tuvork28 <zofi@xamh.de>
2021-10-16 11:40:40 +0200

    Adjusted validate_credentials

--- src/operations/sign_in_user.cr
+++ src/operations/sign_in_user.cr
@@ -20,27 +20,12 @@ class SignInUser < Avram::Operation
 
   # `validate_credentials` determines if a user can sign in.
   #
-  # If desired, you can add additional checks in this method, e.g.
-  #
-  #    if user.locked?
-  #      email.add_error "is locked out"
-  #    end
+  # If desired, you can add additional checks in this method.
   private def validate_credentials(user)
     if user
-      unless user.confirmed?
-        email.add_error "is not confirmed"
-        return
-      end
-
       unless Authentic.correct_password?(user, password.value.to_s)
         password.add_error "is wrong"
-        return
       end
-    else
-      # Usually ok to say that an email is not in the system:
-      # https://kev.inburke.com/kevin/invalid-username-or-password-useless/
-      # https://github.com/luckyframework/lucky_cli/issues/192
-      email.add_error "is not in our system"
     end
   end
 end
