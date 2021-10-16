5bbd4d816b663aed2000b23c2c804044cd9e3b4d
11tuvork28 <zofi@xamh.de>
2021-10-16 13:13:38 +0200

    Created migration for Channels model

---
+++ db/migrations/20211016105853_create_channels.cr
@@ -0,0 +1,16 @@
+class CreateChannels::V20211016105853 < Avram::Migrator::Migration::V1
+  def migrate
+    # Learn about migrations at: https://luckyframework.org/guides/database/migrations
+    create table_for(Channels) do
+      primary_key id : Int64
+      add author : String
+      add deleted : Bool = false
+      add subscribed : Time
+      add_timestamps
+    end
+  end
+
+  def rollback
+    drop table_for(Channels)
+  end
+end
