6c2e3420efb03d7cf72b07d29a91252fc2cfdab8
11tuvork28 <zofi@xamh.de>
2021-10-16 14:20:02 +0200

    cleaned up actions

--- src/actions/sign_up_confirmation_requests/new.cr
+++
@@ -1,7 +0,0 @@
-class SignUpConfirmationRequests::New < BrowserAction
-  include Auth::RedirectSignedInUsers
-
-  get "/sign_up_confirmation" do
-    html SignUpConfirmationRequests::NewPage, operation: RequestSignUpConfirmation.new
-  end
-end
