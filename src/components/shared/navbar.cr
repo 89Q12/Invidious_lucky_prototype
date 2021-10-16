ac1f686aa7ed4872c7ed0688d75bf2551191b160
11tuvork28 <zofi@xamh.de>
2021-10-16 21:06:24 +0200

    Transpiled navbar from ecr to lucky component

--- src/components/shared/navbar.cr
+++ src/components/shared/navbar.cr
@@ -1,79 +1,67 @@
 class Shared::Navbar < BaseComponent
   needs user : User?
-  needs simple : Bool = false
+  needs search : String?
+  needs darktheme : Bool?
 
   def render
-    nav class: "bg-white shadow", data_controller: "toggle" do
-      div class: "max-w-7xl mx-auto px-4 sm:px-6 lg:px-8" do
-        div class: "flex justify-between items-center h-16" do
-          render_left_navbar
-
-          unless simple?
-            render_right_navbar(user)
-          end
-
-          div class: "-mr-2 flex items-center sm:hidden", data_action: "click->toggle#toggle" do
-            render_hamburger_icon
-          end
-        end
-      end
-
-      div class: "hidden sm:hidden", data_toggle_target: "toggleable" do
-        render_hamburger_menu(user)
+    div class: "pure-g navbar h-box" do
+      div class: "flex justify-between items-center h-16" do
+        render_left_navbar(search)
+        render_right_navbar(user, darktheme)
       end
     end
   end
 
-  private def render_left_navbar
-    div class: "flex" do
-      div class: "flex-shrink-0 flex items-center" do
-        render_logo
+  private def render_left_navbar(search : String | Nil)
+    div class: "pure-u-1 pure-u-md-4-24" do
+      render_logo
+    end
+    div class: "pure-u-1 pure-u-md-12-24 searchbar" do
+      form(methode: "get", class: "pure-form", action: "/search") do
+        fieldset do
+          input(type: "text", value: "#{search}", name: "q", style: "width:100%;", attrs: [:required])
+        end
       end
     end
   end
 
   private def render_logo
-    link "Invidious Lucky Prototype", to: Home::Index, class: "leading-8 font-extrabold tracking-tight text-gray-900 text-xl sm:leading-10 hover:text-gray-600"
+    link "Invidious Lucky Prototype", to: Home::Index, class: "index-link pure-menu-heading"
   end
 
-  private def render_hamburger_icon
-    button aria_expanded: "false", aria_label: "Main menu", class: "inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out" do
-      tag "svg", class: "block h-6 w-6", fill: "none", stroke: "currentColor", viewBox: "0 0 24 24" do
-        tag "path", d: "M4 6h16M4 12h16M4 18h16", stroke_linecap: "round", stroke_linejoin: "round", stroke_width: "2"
+  private def render_right_navbar(user : User, dark_theme : Bool | Nil)
+    div class: "pure-u-1 pure-u-md-8-24 user-field" do
+      div class: "pure-u-1-4" do
+        a id: "toggle_theme", href: "/toggle_theme?referer=", class: "pure-menu-heading" do
+          if dark_theme
+            i class: "icon ion-ios-sunny"
+          else
+            i class: "icon ion-ios-moon"
+          end
+        end
       end
-      tag "svg", class: "hidden h-6 w-6", fill: "none", stroke: "currentColor", viewBox: "0 0 24 24" do
-        tag "path", d: "M6 18L18 6M6 6l12 12", stroke_linecap: "round", stroke_linejoin: "round", stroke_width: "2"
+      div class: "pure-u-1-4" do
+        a class: "pure-menu-heading", title: "Subscriptions", id: "notification_ticker" do
+          unless user.nil?
+            notification_count = user.notifications.size
+            if notification_count > 0
+              div id: "notification_count" do
+                text notification_count
+              end
+              i class: "icon ion-ios-notifications"
+            else
+              i class: "icon ion-ios-notifications-outline"
+            end
+          end
+        end
+        mount UI::Button, &.link("Log Out", to: Authentication::Logout, flow_id: "nav-sign-out-button")
       end
     end
   end
 
-  private def render_hamburger_menu(user : User)
-    div class: "pt-2 pb-3" do
-      mount UI::NavbarHamburgerButton, &.link("Home", to: Home::Index, flow_id: "nav-hamburger-home-button")
-      mount UI::NavbarHamburgerButton, &.link("Sign out", to: SignIns::Delete, flow_id: "nav-hamburger-sign-out-button")
-    end
-  end
-
-  private def render_hamburger_menu(no_user : Nil)
-    div class: "pt-2 pb-3" do
-      mount UI::NavbarHamburgerButton, &.link("Home", to: Home::Index, flow_id: "nav-hamburger-home-button")
-      mount UI::NavbarHamburgerButton, &.link("Sign in", to: SignIns::New, flow_id: "nav-hamburger-sign-in-button")
-      mount UI::NavbarHamburgerButton, &.link("Sign up", to: SignUps::New, flow_id: "nav-hamburger-sign-up-button")
-    end
-  end
-
-  private def render_right_navbar(user : User)
-    div class: "hidden sm:flex sm:space-x-2 items-center" do
-      link user.email, to: Me::Edit, class: "text-sm font-medium text-gray-900 hover:text-gray-600"
-
-      mount UI::Button, &.link("Sign Out", to: SignIns::Delete, flow_id: "nav-sign-out-button")
-    end
-  end
-
-  private def render_right_navbar(no_user : Nil)
-    div class: "hidden sm:flex sm:space-x-2 items-center" do
-      mount UI::Button, &.link("Sign In", to: SignIns::New, flow_id: "nav-sign-in-button")
-      mount UI::Button, &.link("Sign Up", to: SignUps::New, flow_id: "nav-sign-up-button")
+  private def render_right_navbar(no_user : Nil, dark_theme : Bool | Nil)
+    div class: "pure-u-1 pure-u-md-8-24 user-field" do
+      mount UI::Button, &.link("Login", to: Authentication::NewLogin, flow_id: "nav-sign-in-button")
     end
   end
 end
