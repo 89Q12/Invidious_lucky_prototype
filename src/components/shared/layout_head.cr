a295ac39e50636ec8f54678d46a7098d0a416245
11tuvork28 <zofi@xamh.de>
2021-10-16 15:35:21 +0200

    So head now works fine I guess

--- src/components/shared/layout_head.cr
+++ src/components/shared/layout_head.cr
@@ -13,12 +13,12 @@ class Shared::LayoutHead < BaseComponent
       meta name: "turbolinks-cache-control", content: "no-cache"
       meta name: "theme-color", content: "#575757"
       meta name: "msapplication-TileColor", content: "#575757"
-      empty_tag "Invidious", type: "application/opensearchdescription+xml", rel: "search", href: "/opensearch.xml"
-      empty_tag rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png?v=#{@assert_commits}"
-      empty_tag rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png?v=#{@assert_commits}"
-      empty_tag rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16x16.png?v=#{@assert_commits}"
-      empty_tag rel: "manifest", href: "/site.webmanifest?v=#{@assert_commits}"
-      empty_tag rel: "mask-icon", href: "/safari-pinned-tab.svg?v=#{@assert_commits}", color: "#575757"
+      empty_tag "link", titel: "Invidious", type: "application/opensearchdescription+xml", rel: "search", href: "/opensearch.xml"
+      empty_tag "link", rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png?v=#{@assert_commits}"
+      empty_tag "link", rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png?v=#{@assert_commits}"
+      empty_tag "link", rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16x16.png?v=#{@assert_commits}"
+      empty_tag "link", rel: "manifest", href: "/site.webmanifest?v=#{@assert_commits}"
+      empty_tag "link", rel: "mask-icon", href: "/safari-pinned-tab.svg?v=#{@assert_commits}", color: "#575757"
       csrf_meta_tags
       responsive_meta_tag
     end
