12fa9231cd24e1b936b9c1e0aadec2c97f41fd65
11tuvork28 <zofi@xamh.de>
2021-10-17 11:17:24 +0200

    Furhter cleanup of the template

--- src/actions/me/edit.cr
+++
@@ -1,7 +0,0 @@
-class Me::Edit < BrowserAction
-  get "/me" do
-    html Me::EditPage,
-      operation: SaveUserSettings.new(current_user),
-      user: current_user
-  end
-end
--- src/actions/sign_ins/delete.cr
+++
@@ -1,6 +0,0 @@
-class SignIns::Delete < BrowserAction
-  delete "/sign_out" do
-    sign_out
-    redirect to: SignIns::New
-  end
-end
