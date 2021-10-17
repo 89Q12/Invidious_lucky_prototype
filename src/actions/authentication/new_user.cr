ed5d25846a546eb74b79a376e38d12970e7a17e9
11tuvork28 <zofi@xamh.de>
2021-10-17 12:51:31 +0200

    Added correct params

--- src/actions/authentication/new_user.cr
+++ src/actions/authentication/new_user.cr
@@ -2,6 +2,6 @@ class Authentication::NewUser < BrowserAction
   include Auth::RedirectSignedInUsers
 
   get "/sign_up" do
-    html Authentication::SignUp, operation: SignUpUser.new
+    html Authentication::SignUp, operation: SignUpUser.new, assert_commits: "", modified_source_code_url: "modified_source_code_url", version_string: "version_string"
   end
 end
