ebda2f9417485bd76d7550f1eea115060a745a9f
11tuvork28 <zofi@xamh.de>
2021-10-23 14:59:20 +0200

    layed out how preferences would be assigned

--- src/operations/sign_up_user.cr
+++ src/operations/sign_up_user.cr
@@ -2,18 +2,21 @@ class SignUpUser < User::SaveOperation
   param_key :user
   # Change password validations in src/operations/mixins/password_validations.cr
   include PasswordValidations
-
   permit_columns userid
   attribute password : String
   attribute password_confirmation : String
-
   before_save do
     validate_uniqueness_of userid
     assign_confirmation_token
+    # assign_preferences
     Authentic.copy_and_encrypt password, to: encrypted_password
   end
 
   private def assign_confirmation_token
     token.value = Random::Secure.urlsafe_base64(32)
+  end
+
+  private def assign_preferences
+    # preferences.value = Preferences.new would go here
   end
 end
