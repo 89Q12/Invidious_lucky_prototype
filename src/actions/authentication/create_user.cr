4c20620f539415f36ba7683083e3ae7dee12187f
11tuvork28 <zofi@xamh.de>
2021-10-23 15:00:21 +0200

    Removed unsued code

--- src/actions/authentication/create_user.cr
+++ src/actions/authentication/create_user.cr
@@ -4,10 +4,8 @@ class Authentication::CreateUser < BrowserAction
   post "/sign_up" do
     SignUpUser.create(params) do |operation, user|
       if user
-        flash.info = "Thanks for signing up."
         redirect Feed::Index
       else
-        flash.info = "Couldn't sign you up"
         html Authentication::SignUp, operation: operation, assert_commits: "", modified_source_code_url: "modified_source_code_url", version_string: "version_string"
       end
     end
